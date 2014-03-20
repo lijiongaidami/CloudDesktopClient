c070fe66a2ffbdb5887df9dafdd124ca512f92de
hjy <hjy@hjy-desktop>
2014-03-20 14:16:47 +0800

    修改bug，上一版本有错误

--- CloudDesktopClient/src/com/hjy/net/Rdesktop.java
+++ CloudDesktopClient/src/com/hjy/net/Rdesktop.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class Rdesktop {
 
 	public static void runRdesktop(String name, String password, String ip) {
-		String shellPath = System.getProperty("usr.dir")
+		String shellPath = System.getProperty("user.dir")
 				+ "/shellResource/rdp.sh";
-		String cmd = String.format("bash %s %s %s %s", shellPath, ip, name,
-				password);
+		String cmd = String.format("bash %s %s %s %s", shellPath,
+				"192.168.1.139", name, password);
 		try {
 			Runtime.getRuntime().exec(cmd);
 		} catch (IOException e) {
